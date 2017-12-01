b781a7a1e7eac7167d9ccb782f61ab6ffeefe596
zhongdaxin <zzh@daxinpj.com>
2017-12-01 17:13:04 +0800

    Asynchronized task executor

---
+++ src/main/java/com/zzh/bootdemo/AsyncApp.java
@@ -0,0 +1,26 @@
+package com.zzh.bootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@SpringBootApplication
+@EnableAsync
+public class AsyncApp {
+    public static void main(String[] args) {
+        SpringApplication.run(AsyncApp.class, args).close();
+    }
+
+    public Executor asyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(2);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("GithubLooup-");
+        return executor;
+    }
+
+}
---
+++ src/main/java/com/zzh/bootdemo/AsyncRunner.java
@@ -0,0 +1,35 @@
+package com.zzh.bootdemo;
+
+import com.zzh.bootdemo.model.User;
+import com.zzh.bootdemo.service.GitHubLookupService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CompletableFuture;
+
+@Component
+public class AsyncRunner implements CommandLineRunner {
+    private static final Logger logger = LoggerFactory.getLogger(AsyncRunner.class);
+    private final GitHubLookupService gitHubLookupService;
+
+    public AsyncRunner(GitHubLookupService gitHubLookupService) {
+        this.gitHubLookupService = gitHubLookupService;
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+        long start = System.currentTimeMillis();
+        CompletableFuture<User> u1 = gitHubLookupService.findUser("PivotalSoftware");
+        CompletableFuture<User> u2 = gitHubLookupService.findUser("CloudFoundry");
+        CompletableFuture<User> u3 = gitHubLookupService.findUser("Spring-Projects");
+
+        // Wait until they are all done
+        CompletableFuture.allOf(u1, u2, u3).join();
+        logger.info("Elapsed time: " + (System.currentTimeMillis() - start));
+        logger.info("-->" + u1.get());
+        logger.info("-->" + u2.get());
+        logger.info("-->" + u3.get());
+    }
+}
